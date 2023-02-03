7cc814fb8d6f662fced2150ae50fdbf8c6977468
DigitalTwelfthMan <JamesBotterill@users.noreply.github.com>
2023-02-03 22:08:24 +0000

    update

--- index.js
+++ index.js
@@ -3,35 +3,40 @@ const fs = require('fs');
 const convert = require('heic-convert');
 const uri2path = require('file-uri-to-path')
 
-module.exports = (job, settings, action, type) => {
-    console.log('hello from my module: ' + action.module);
-
+module.exports = (job, settings, options, type) => {
+    //console.log('hello from my module: ' + action.module);
+    console.log("convert module started")
     return new Promise((resolve, reject) => {
         options.layers.forEach(asset => {
 
             const index = job.assets.findIndex(x => x.layerName == asset);
             const imageFile = job.assets[index].src
+            console.log(imageFile)
             const filepath = uri2path(imageFile)
+            console.log(filepath)
             const fileExt = path.extname(filepath)
             const dir = options.dir || path.dirname(filepath)
+            const filename = path.basename(filepath, fileExt)
+            const newFile = dir + filename + '.png' 
 
-            if(fileExt === '.heic'){
-                convertFile(filepath, dir)
-            }
+            if(fileExt === '.HEIC'|| '.heic'){
+                async ()=>{
+                    await convertFile(filepath, filename, fileExt,  dir)
+                    job.assets[index].src = newFile
+                    settings.logger.log(`changed ${job.assets[index].layerName} value to ${newFile}`)};
+                }
         })
-        resolve(job)
+        resolve()
     })
     
 };
 
-const convertFile  = async (file, outputDir) => {
-    let filename = path.basename(file, '.hiec')
-    const inputBuffer = await promisify(fs.readFile)(file);
+const convertFile  = async (file, name, ext ,outputDir) => {
+    const inputBuffer = await promisify(fs.readFile)(file, ext);
     const outputBuffer = await convert({
       buffer: inputBuffer, // the HEIC file buffer
       format: 'PNG'        // output format
     });
   
-    await promisify(fs.writeFile)(outputDir + filename + '.png', outputBuffer);
+    await promisify(fs.writeFile)(outputDir + name + '.png', outputBuffer);
   };
-
