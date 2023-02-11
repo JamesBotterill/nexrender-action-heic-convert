e4cb569063af40a701f52ad451e030a2385623a8
DigitalTwelfthMan <JamesBotterill@users.noreply.github.com>
2023-02-11 12:42:00 +0000

    fix

--- index.js
+++ index.js
@@ -1,36 +1,54 @@
 const fs = require('fs');
-const convert = require('heic-convert');
+const convert = require('heic-convert')
 const uri2path = require('file-uri-to-path')
-const path = require('path');
+const { promisify } = require('util');
+const path = require('path')
 
-module.exports = (job, settings, options, type) => {
-    //console.log('hello from my module: ' + action.module);
-    console.log("convert module started")
-    return new Promise((resolve, reject) => {
-        options.layers.forEach(asset => {
+module.exports = (job, settings, {layer, dir}, type) => {
+    const promises = layer.map(asset => {
+        // get the file path, file name and extension
+        const index = job.assets.findIndex(x => x.layerName == asset);
+        const imageFile = job.assets[index].src
+        const filepath = uri2path(imageFile)
+        const fileExt = path.extname(filepath)
+        const filename = path.basename(filepath, fileExt)
 
-            const index = job.assets.findIndex(x => x.layerName == asset);
-            const imageFile = job.assets[index].src
-            const filepath = uri2path(imageFile)
-            const fileExt = path.extname(filepath)
-            const dir = options.dir || path.dirname(filepath)
-            const filename = path.basename(filepath, fileExt)
-            const newFile = dir + filename + '.png' 
+        // get the directory of the file
+        const outputDir = dir || path.dirname(filepath)
 
-            if(fileExt === '.HEIC'|| '.heic'){
-                    async (file, name, ext ,outputDir) => {
-                        const inputBuffer = await fs.readFile(file, ext);
-                        const outputBuffer = await convert({
-                          buffer: inputBuffer, // the HEIC file buffer
-                          format: 'PNG'        // output format
-                        });
-                      
-                        await fs.writeFile(outputDir + name + '.png', outputBuffer);
-                        job.assets[index].src = newFile
-                        settings.logger.log(`changed ${job.assets[index].layerName} value to ${newFile}`)
-                    };
-            };
+        // create a new file path for the PNG file
+        const newFile = outputDir + filename + '.png'
+
+        // if the file is not a HEIC file, return the asset
+        if (fileExt.toLowerCase() != '.heic') {
+            return Promise.resolve(asset)
+        }
+        // convert the HEIC file to PNG
+        return new Promise(async (resolve, reject) => {
+            convert({
+                buffer: await promisify(fs.readFile)(filepath), // the HEIC file buffer
+                format: 'PNG'                       // output format
+            }).then (outputBuffer => {
+                // write the PNG file
+                fs.writeFile(newFile, outputBuffer)
+
+                // set the new file path to the asset
+                asset.src = newFile
+
+                resolve(asset)
+            }).catch(err => {
+                reject(err)
+            })
         })
-        resolve()
+    })
+
+    return new Promise((resolve, reject) => {
+        Promise.all(promises).then(assets => {
+            // set the new assets to the job
+            job.assets = assets
+            resolve(job)
+        }).catch(err => {
+            reject(err)
+        })
     })
 };
